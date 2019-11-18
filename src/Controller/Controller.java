83fbf58f0c4c3e4c29e59c6d66f8c4a4d9bcf039
Vadim <9536611@gmail.com>
2019-11-18 18:14:25 +0400

    Controller methods

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,5 +1,41 @@
 package Controller;
 
+import Model.RepositoryModel;
+import Utils.Track;
+
 public class Controller {
 
+    private RepositoryModel model;
+
+    public Controller(RepositoryModel model) {
+        this.model = model;
+    }
+
+    public void loadData(){
+
+    }
+
+    public void saveData(){
+
+    }
+
+    public void addTrack(Track track){
+
+    }
+
+    public void deleteTrack(Track track){
+
+    }
+
+    public void changeTrack(Track track){
+
+    }
+
+    public void browsingTrack(){
+
+    }
+
+    public void updateModel(){
+
+    }
 }
